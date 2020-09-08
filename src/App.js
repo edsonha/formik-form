22e730276f668eb6528a2837bfb0c321b6b01506
edsonha <edsonha@gmail.com>
2020-09-08 22:11:43 +0800

    Add password field and handleSubmit to console log the values

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import React from "react";
 import { withFormik } from "formik";
 
-function App({ values, handleChange }) {
+function App({ values, handleChange, handleSubmit }) {
   return (
-    <input
-      value={values.email}
-      type="email"
-      name="email"
-      placeholder="Email"
-      onChange={handleChange}
-    />
+    <form onSubmit={handleSubmit}>
+      <input
+        value={values.email}
+        type="email"
+        name="email"
+        placeholder="Email"
+        onChange={handleChange}
+      />
+      <input
+        value={values.password}
+        type="password"
+        name="password"
+        placeholder="Password"
+        onChange={handleChange}
+      />
+      <button type="submit">Submit</button>
+    </form>
   );
 }
 
 const FormikApp = withFormik({
-  mapPropsToValues({ email }) {
+  mapPropsToValues({ email, password }) {
     return {
       email: email || "default@email.com",
+      password: password || "defaultPassword",
     };
+  },
+  handleSubmit(values) {
+    console.log(values);
   },
 })(App);
 
