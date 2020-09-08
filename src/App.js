bd2677fe83a00c10a5c2d2a5d2546699b660b62e
edsonha <edsonha@gmail.com>
2020-09-08 22:57:33 +0800

    Add validation schema and add custom error message and have touched property

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import { withFormik, Form, Field } from "formik";
+import * as Yup from "yup";
 
-function App({ values }) {
+function App({ values, errors, touched }) {
   return (
     <Form>
-      <Field type="email" name="email" placeholder="Email" />
-      <Field type="password" name="password" placeholder="Password" />
+      <div>
+        {touched.email && errors.email && <p>{errors.email}</p>}
+        <Field type="email" name="email" placeholder="Email" />
+      </div>
+      <div>
+        {touched.password && errors.password && <p>{errors.password}</p>}
+        <Field type="password" name="password" placeholder="Password" />
+      </div>
       <label>
         <Field type="checkbox" name="newsletter" checked={values.newsletter} />
         Join our newsletter
@@ -29,6 +36,14 @@ const FormikApp = withFormik({
       plan: plan || "premium",
     };
   },
+  validationSchema: Yup.object().shape({
+    email: Yup.string()
+      .email("Email is not valid custom")
+      .required("Email is required custom"),
+    password: Yup.string()
+      .min(9, "Password not long custom")
+      .required("Password is required custom"),
+  }),
   handleSubmit(values) {
     console.log(values);
   },
