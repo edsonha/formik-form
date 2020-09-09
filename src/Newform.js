0d4d2bbe5c6d6c16790ff53662b705e880d6c4d3
edsonha <edsonha@gmail.com>
2020-09-09 08:40:24 +0800

    Create simple form that display initial value

--- src/Newform.js
+++ src/Newform.js
@@ -1,7 +1,29 @@
 import React from "react";
+import { Formik } from "formik";
+import { TextField } from "@material-ui/core";
 
 function NewForm() {
-  return "Hello World";
+  return (
+    <div>
+      <Formik
+        initialValues={{ firstName: "bob" }}
+        onSubmit={(data) => {
+          console.log(data);
+        }}
+      >
+        {({ values, handleChange, handleBlur, handleSubmit }) => (
+          <form onSubmit={handleSubmit}>
+            <TextField
+              name="firstName"
+              value={values.firstName}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+          </form>
+        )}
+      </Formik>
+    </div>
+  );
 }
 
 export default NewForm;
